83a84561f4e5157e14a0a89de65291b8cdaba306
pranava007 <pranavamuthu000@gmail.com>
2024-10-21 04:31:30 +0530

    resuma download completed

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Button } from 'flowbite-react'; // Ensure flowbite-react is installed
 
-
 const Download = () => {
 
-  const downloadResume = () => {
-    // Trigger a download programmatically
-    const link = document.createElement("a");
-    link.href = "pranavamuthu.pdf"; // The path to your PDF file in the public folder
-    link.download = "resuma.pdf"; // The filename for the downloaded file
-    link.click();
-  };
-
+  const downloadResume = () =>{
+    const link = document.createElement("a")
+    link.href="/pranavamuth.pdf"
+    link.download="pranavamuhtu_resume.pdf"
+    link.click()
+  }
 
   return (
-   
-      <Button onClick={downloadResume} className='bg-gradient-to-r from-teal-400 to-blue-500'>
-        Download CV
-      </Button>
-    
+    <Button onClick={downloadResume} className="bg-gradient-to-r from-teal-400 to-blue-500">
+      Download CV
+    </Button>
   );
 };
 
